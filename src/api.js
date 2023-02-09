b53a4c55e7eef6fee4c1d7897daf28dab4e22613
deepanshu chugh <deepanshuchugh.dc@gmail.com>
2023-02-09 15:04:48 +0530

    updated api.js

--- src/api.js
+++ src/api.js
@@ -13,7 +13,7 @@ export  async function getCurrWeather(lon,lat,unit){
 
     export  async function getCityWeather(cityName,unit){
 
-        const response= await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=${unit}&APPID=43787cd95d052d4c8e35e1b99cab3e01`);
+        const response= await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=${unit}&APPID=${API_KEY}`);
        if(!response.ok){
       throw { message: 'Failed to get temp.', status: 500 };
     
